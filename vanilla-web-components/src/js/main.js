75aa6e9d8ae785b625e631d2396273fa4eaae117
Alex <alex@alex-mb-pro.lan>
2019-07-03 11:35:36 +0200

    Add CSS transition.

--- vanilla-web-components/src/js/main.js
+++ vanilla-web-components/src/js/main.js
@@ -4,3 +4,11 @@ const modal = document.querySelector('pn-modal');
 confirmBtn.addEventListener('click', () => {
   modal.open();
 })
+
+modal.addEventListener('confirmOrder', () => {
+  console.log('Confirmed!');
+})
+
+modal.addEventListener('cancelOrder', () => {
+  console.log('Canceled!');
+})
