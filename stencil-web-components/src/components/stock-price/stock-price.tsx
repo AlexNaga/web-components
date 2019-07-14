fd41c1365ff09fba2ef82f7ec9feb1534f9411d6
Alex <alex.naga@hotmail.com>
2019-07-14 17:23:55 +0200

    Change styling

--- stencil-web-components/src/components/stock-price/stock-price.tsx
+++ stencil-web-components/src/components/stock-price/stock-price.tsx
@@ -1,4 +1,4 @@
-import { Component } from '@stencil/core';
+import { Component, h } from '@stencil/core';
 
 import config from './config/config.json';
 const API_KEY = config.API_KEY;
@@ -12,5 +12,22 @@ export class StockPrice {
   componentWillLoad() {
     console.log('Hello, World!');
     console.log(API_KEY);
+  }
+
+  onFetchStockPrice(event: Event) {
+    event.preventDefault();
+    console.log('Submitted!');
+  }
+
+  render() {
+    return [
+      <form onSubmit={this.onFetchStockPrice.bind(this)}>
+        <input id='stock-symbol' />
+        <button type='submit'>Fetch</button>
+      </form>,
+      <div>
+        <p>Price: {0}</p>
+      </div>
+    ]
   }
 }
